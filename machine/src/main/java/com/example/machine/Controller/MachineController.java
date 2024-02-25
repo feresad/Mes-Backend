e5cacbca02f36c23690f70946f975cd09ef6e51a
feresad <feresadouani10@gmail.com>
2024-02-25 21:04:13 +0100

    fix machine

--- machine/src/main/java/com/example/machine/Controller/MachineController.java
+++ machine/src/main/java/com/example/machine/Controller/MachineController.java
@@ -4,16 +4,10 @@ import com.example.machine.entity.Machine;
 import com.example.machine.repository.MachineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-@RefreshScope
 @RequestMapping("machines")
 public class MachineController {
      @Autowired
@@ -22,5 +16,23 @@ public class MachineController {
     @GetMapping("/all")
     public Iterable<Machine> getAllMachines(){
         return MachineRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Machine getMachinebyId(@PathVariable(name = "id") Long id){
+        return MachineRepository.findById(id).get();
+    }
+    @PostMapping("/add")
+    public Machine addMachine(@RequestBody Machine machine){
+        machine.setEtat(true);
+        return MachineRepository.save(machine);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteMachine(@PathVariable(name = "id") Long id){
+        MachineRepository.deleteById(id);
+    }
+    @PutMapping("/{id}")
+    public Machine updateMachine(@PathVariable(name = "id") Long id, @RequestBody Machine machine){
+        machine.setId(id);
+        return MachineRepository.save(machine);
     }
 }
--- machine/src/main/java/com/example/machine/MachineApplication.java
+++ machine/src/main/java/com/example/machine/MachineApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@CrossOrigin(origins = "http://localhost:4200")
 public class MachineApplication {
 
 	public static void main(String[] args) {
